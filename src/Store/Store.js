0f0280234099e1d59fa5e52c319d65073c5c527a
Nihal-Sharma <72800662+Nihal-Sharma@users.noreply.github.com>
2024-05-02 18:08:11 +0530

    minor

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -9,11 +9,11 @@ const successLogin = ()=>{
     localStorage.setItem("LOGGEDIN" , "true")
 }
 
-// const endpoint = "https://bubble-backend-nine.vercel.app"
-const localapi = "http://127.0.0.1:4000"
+const endpoint = "https://bubble-backend-nine.vercel.app"
+// const localapi = "http://127.0.0.1:4000"
 
 export const useStore = create ((set) =>({
-    apiendpoint : localapi,
+    apiendpoint : endpoint,
     loggedin : loggedcheck(),
     login : ()=>set((state)=>({ loggedin :'true'  ,successLogin }))
 }))
